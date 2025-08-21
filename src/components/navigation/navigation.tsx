df70d26b39ce24a76c0ed1579fc523a23c7d1642
VLGKiwi <zvonarev.dev@gmail.com>
2025-08-21 18:16:05 +0300

    feat: сделал заготовку для бургера

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,43 +1,70 @@
 "use client"
 
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import classNames from 'classnames'
 
 import styles from './navigation.module.scss'
 import { NavigationProps } from './navigation.types'
 
-const Navigation: FC<NavigationProps> = ({ className }) => {
+import BurgerIcon from '@icons/burger.svg'
+
+const Navigation: FC<NavigationProps> = ({ className, isMobile }) => {
   const rootClassName = classNames(styles.root, className)
 
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <ul className={rootClassName}>
-      <li className={styles.item}><a href="/price" className={styles.link}>Тарифы</a></li>
+    !isMobile ? (
+      <>
+        <ul className={rootClassName}>
+          <li className={styles.item}><a href="/price" className={styles.link}>Тарифы</a></li>
 
-      <li className={classNames(styles.item, styles.dropdown)}>
-        <button className={styles.trigger} type="button" onClick={() => {
-          window.location.href = '/partners'
-        }}>
-          Партнёрам <span className={styles.caret} aria-hidden>▾</span>
-        </button>
-        <ul className={styles.menu}>
-          <li className={styles.menuItem}><a className={styles.menuLink} href="/inviteFriend">Пригласи друга</a></li>
-          <li className={styles.menuItem}><a className={styles.menuLink} href="/partners">Партнерская программа</a></li>
-        </ul>
-      </li>
+          <li className={classNames(styles.item, styles.dropdown)}>
+            <button className={styles.trigger} type="button" onClick={() => {
+              window.location.href = '/partners'
+            }}>
+              Партнёрам <span className={styles.caret} aria-hidden>▾</span>
+            </button>
+            <ul className={styles.menu}>
+              <li className={styles.menuItem}><a className={styles.menuLink} href="/inviteFriend">Пригласи друга</a></li>
+              <li className={styles.menuItem}><a className={styles.menuLink} href="/partners">Партнерская программа</a></li>
+            </ul>
+          </li>
 
-      <li className={classNames(styles.item, styles.dropdown)}>
-        <button className={styles.trigger} type="button" onClick={() => {
-          window.location.href = '/about'
-        }}>
-          О нас <span className={styles.caret} aria-hidden>▾</span>
-        </button>
-        <ul className={styles.menu}>
-          <li className={styles.menuItem}><a className={styles.menuLink} href="/company">О компании</a></li>
-          <li className={styles.menuItem}><a className={styles.menuLink} href="/contacts">Контакты</a></li>
-          <li className={styles.menuItem}><a className={styles.menuLink} href="/blog">Блог</a></li>
+          <li className={classNames(styles.item, styles.dropdown)}>
+            <button className={styles.trigger} type="button" onClick={() => {
+              window.location.href = '/about'
+            }}>
+              О нас <span className={styles.caret} aria-hidden>▾</span>
+            </button>
+            <ul className={styles.menu}>
+              <li className={styles.menuItem}><a className={styles.menuLink} href="/company">О компании</a></li>
+              <li className={styles.menuItem}><a className={styles.menuLink} href="/contacts">Контакты</a></li>
+              <li className={styles.menuItem}><a className={styles.menuLink} href="/blog">Блог</a></li>
+            </ul>
+          </li>
         </ul>
-      </li>
-    </ul>
+      </>
+    ) : (
+      <>
+        <div className={styles.burger} onClick={() => setIsOpen(!isOpen)}>
+          <span className={styles.burgerIcon}>
+            <BurgerIcon />
+          </span>
+        </div>
+        {
+          isOpen && (
+            <div className={styles.root}>
+              <div className={styles.wrapper}>
+                <div className={styles.menu}>
+
+                </div>
+              </div>
+            </div>
+          )
+        }
+      </>
+    )
   )
 }
 
