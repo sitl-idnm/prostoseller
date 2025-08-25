9d7e9ca7d67f521b74d3864af7d3c4f12617f1de
VLGKiwi <zvonarev.dev@gmail.com>
2025-08-25 14:46:38 +0300

    fix: form styles

--- src/views/company/company.tsx
+++ src/views/company/company.tsx
@@ -6,6 +6,9 @@ import { CompanyProps } from './company.types'
 import { Content } from '@/modules/content'
 import { Wrapper } from '@/ui/wrapper'
 import { TitleHandler } from '@/modules/titleHandler'
+import { Button } from '@/ui'
+import ArrowWhiteIcon from '@icons/arrowWhite.svg'
+import { LINKS } from '@/shared/const'
 
 const Company: FC<CompanyProps> = ({
   className
@@ -17,19 +20,22 @@ const Company: FC<CompanyProps> = ({
       <Wrapper className={styles.wrapper}>
         <Content
           variant="split"
-          backgroundRoot="#D9E3FF"
+          backgroundRoot="transparent"
+          backgroundContent="#fff url('/images/company_bg.png') no-repeat 300px 80px / contain"
           title="О компании"
           titleTagName="h1"
           titleSize="md"
-          subtitle="Prostoseller – это сервис, который показывает реальный доход и экономику продаж на Wildberries и Ozon "
-
+          subtitle={<><span className={styles.purple}>Prostoseller</span> – это сервис, который показывает реальный <span className={styles.purple}>доход</span> и экономику продаж на <span className={styles.purple}>Wildberries</span> и <span className={styles.ozon}>Ozon</span></>}
+          buttons={<Button as="a" isRouteLink href={LINKS.connectFree} variant="gradient" buttonWidth="100%" icon={<ArrowWhiteIcon />}>Подключить бесплатно</Button>}
+          imageSrc="/images/photo_team.png"
+          imageAlt="Команда Prostoseller"
         />
         <TitleHandler
-          title="НАША ЦЕЛЬ – сделать прибыль селлеров больше, а бизнес понятнее!"
+          title={<><span className={styles.purple}>НАША ЦЕЛЬ</span> – сделать прибыль селлеров больше, а бизнес понятнее!</>}
           titleTagName="h2"
           titleSize="lg"
           mark={<>Наша миссия</>}
-          description="Мы, опытные продавцы на OZON и Wildberries со стажем более 5 лет, поняли, как тяжело разобраться в множестве цифр, чтобы оценить реальную эффективность и доходность продаж на российских маркетплейсах. Глядя на то, как другие продавцы ведут бизнес «в минус», приняли решение изменить эту ситуацию и создали Prostoseller. Теперь каждый селлер точно понимает, сколько зарабатывает в разрезе каждого товара и принимает самые верные решения по ассортименту и оптимизации расходов!"
+          description={<><span style={{fontWeight: 400}}>Мы, опытные продавцы на OZON и Wildberries со стажем более 5 лет, поняли, как тяжело разобраться в множестве цифр, чтобы оценить реальную эффективность и доходность продаж на российских маркетплейсах. Глядя на то, как другие продавцы ведут бизнес «в минус», приняли решение изменить эту ситуацию и создали Prostoseller. Теперь каждый селлер точно понимает, сколько зарабатывает в разрезе каждого товара и принимает самые верные решения по ассортименту и оптимизации расходов!</span></>}
         />
       </Wrapper>
     </main>
--- src/views/inviteFriend/inviteFriend.tsx
+++ src/views/inviteFriend/inviteFriend.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/ui'
 import { LINKS } from '@/shared/const'
 import { TitleHandler } from '@/modules/titleHandler'
 import { LogoCarousel } from '@/modules/logoCarousel'
+import ArrowWhiteIcon from '@icons/arrowWhite.svg'
 
 const InviteFriend: FC<InviteFriendProps> = ({
   className
@@ -19,16 +20,21 @@ const InviteFriend: FC<InviteFriendProps> = ({
     <main className={rootClassName}>
       <Wrapper className={styles.wrapper}>
         <Content
-          variant="solid"
-          backgroundRoot="#D9E3FF"
-          title="Приглашайте друзей в Prostoseller и пользуйтесь сервисом бесплатно"
+          variant="split"
+          backgroundRoot="url('/images/prostoseller_free.png') no-repeat center center / cover"
+          backgroundContent='transparent'
+          title={<>Приглашайте друзей в <span className={styles.purple}>Prostoseller</span> и пользуйтесь сервисом бесплатно</>}
+          titleTagName="h1"
+          titleSize="sm"
           subtitle="За каждого друга - по одному бесплатному месяцу тебе и ему"
           description="Публикуйте свой пригласительный код в социальных сетях, делитесь кодом с друзьями и получайте свой бонус за каждое его использование"
           subDescription="Пригласил 12 друзей - ДЕРЖИ ГОД БЕСПЛАТНОЙ АНАЛИТИКИ"
-          buttons={<Button as="a" isRouteLink href={LINKS.invite}>Пользоваться бесплатно</Button>}
+          buttons={<Button as="a" isRouteLink href={LINKS.invite ?? LINKS.connectFree} style={{ width: '100%' }} variant="gradient" icon={<ArrowWhiteIcon />}>Пользоваться бесплатно</Button>}
+          imageSrc="/images/box_free.png"
+          imageAlt="Скриншот кабинета"
         />
         <TitleHandler
-          title="Наши партнеры"
+          title={<>Наши <span className={styles.purple}>партнеры</span></>}
           titleTagName="h2"
           titleSize="lg">
           <LogoCarousel />
